c12e77f83b4f24ed7deea59f9201b5d975287113
wisehero10 <godjw96@gmail.com>
2022-09-05 06:42:30 +0900

    Django Admin

--- houses/admin.py
+++ houses/admin.py
@@ -5,4 +5,13 @@ from .models import House
 # Register your models here.
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    list_display = [
+        "name",
+        "price_per_night",
+        "address",
+        "pets_allowed"
+    ]
+
+    list_filter = ["price_per_night", "pets_allowed"]
+    search_fields = ["address"]
+
