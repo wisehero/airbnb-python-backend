0ebeddfd9aa2cd20f4b704ce0252d0fb53a25102
wisehero10 <godjw96@gmail.com>
2022-10-05 16:34:42 +0900

    # Custom Admin

--- users/admin.py
+++ users/admin.py
@@ -6,4 +6,33 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        ("Profile"
+         , {"fields": ("username", "password", "name", "email", "is_host"),
+            "classes": ("wide",)
+            }
+         )
+        ,
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": ("last_login", "date_joined"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    list_display = ("username", "email", "name", "is_host")
