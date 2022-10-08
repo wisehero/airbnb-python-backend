cc362b8f69b11ea89ac4743d9c89f35126c04eb4
wisehero10 <godjw96@gmail.com>
2022-10-08 16:46:16 +0900

    # Rooms Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Room, Amenity
+
 
 # Register your models here.
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+        "created_at",
+        "updated_at",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
--- rooms/models.py
+++ rooms/models.py
@@ -11,6 +11,7 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = "shared_room", "Shared Room"
 
+    name = models.CharField(max_length=180, default="")
     country = models.CharField(max_length=50, default="한국")
     city = models.CharField(max_length=80, default="서울")
     price = models.PositiveIntegerField()
@@ -29,8 +30,17 @@ class Room(CommonModel):
     )
     amenities = models.ManyToManyField("rooms.Amenity")
 
+    def __str__(self):
+        return self.name
+
 
 class Amenity(CommonModel):
     """ Amenity Definition """
     name = models.CharField(max_length=150)
-    description = models.CharField(max_length=150, null=True)
+    description = models.CharField(max_length=150, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
