888176c31088ffa371df1e6bf08cec3640ac1eb6
wisehero10 <godjw96@gmail.com>
2022-10-08 18:11:43 +0900

    # Reviews

---
+++ reviews/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Review
+
+
+# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "payload",
+    )
+    list_filter = ("rating",)
---
+++ reviews/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from common.models import CommonModel
+
+
+# Create your models here.
+
+class Review(CommonModel):
+    """ Review from a User to a Room or Experience """
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE
+    )
+    payload = models.TextField()
+    rating = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user} / {self.rating}"
