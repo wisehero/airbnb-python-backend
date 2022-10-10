71743f59ca0b6acdc465477989901e33d7a8e2f8
wisehero10 <godjw96@gmail.com>
2022-10-10 15:44:01 +0900

    # Custom Filters

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,10 +3,36 @@ from .models import Review
 
 
 # Register your models here.
+
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
