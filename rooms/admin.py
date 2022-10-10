748146e5334159283633ef3b4c4617a21c2e50cb
wisehero10 <godjw96@gmail.com>
2022-10-10 15:28:51 +0900

    # Admin Actions

--- rooms/admin.py
+++ rooms/admin.py
@@ -4,8 +4,16 @@ from .models import Room, Amenity
 
 # Register your models here.
 
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
     list_display = (
         "name",
         "price",
