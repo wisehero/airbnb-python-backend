9887097bde26da0e5067e84a7835c76333f654d7
wisehero10 <godjw96@gmail.com>
2022-10-18 00:33:51 +0900

    # Install Django Rest Framework
    poetry add djangorestframework

--- config/settings.py
+++ config/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 
 CUSTOM_APPS = [
     "common.apps.CommonConfig",
@@ -50,7 +53,7 @@ SYSTEM_APPS = [
     'django.contrib.staticfiles'
 ]
 
-INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
