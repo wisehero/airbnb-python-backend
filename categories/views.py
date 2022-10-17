44bd1d4a744dc69bc843c76145f2be9471b84be3
wisehero10 <godjw96@gmail.com>
2022-10-18 01:17:38 +0900

    # Serializer

--- categories/views.py
+++ categories/views.py
@@ -1,15 +1,18 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Category
+from .serializers import CategorySerializer
 
 
 # Create your views here.
 
 @api_view()
 def categories(request):
+    all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
     return Response(
         {
             "ok": True,
-            "categories": Category.objects.all(),
+            "categories": serializer.data
         }
     )
