cfb2444d2b9fcd014aab18cb58fcd25327099458
wisehero10 <godjw96@gmail.com>
2022-10-24 20:46:21 +0900

    # Validate Booking

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.utils import timezone
 from django.conf import settings
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError, PermissionDenied
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -6,6 +7,8 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
 
+from bookings.models import Booking
+from bookings.serializers import PublicBookingSerializer
 from categories.models import Category
 from medias.serializers import PhotoSerializer
 from reviews.serializers import ReviewSerializer
@@ -233,3 +236,24 @@ class RoomPhotos(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+
+
+class RoomBookings(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        now = timezone.localtime(timezone.now()).date()
+        bookings = Booking.objects.filter(
+            room=room,
+            kind=Booking.BookingKindChoices.ROOM,
+            check_in__gt=now,
+        )
+        serializer = PublicBookingSerializer(bookings, many=True)
+        return Response(serializer.data)
