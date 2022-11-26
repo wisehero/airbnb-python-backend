339b4ea91b2aff8c1cdcd222b7fe5b08084469b6
wisehero10 <godjw96@gmail.com>
2022-11-27 00:15:51 +0900

    # NotFound 페이지

--- airbnb-clone-frontend/src/router.tsx
+++ airbnb-clone-frontend/src/router.tsx
@@ -1,22 +1,19 @@
 import {createBrowserRouter} from "react-router-dom";
 import Root from "./components/Root";
 import Home from "./routes/Home";
-import Users from "./routes/Users";
-
+import NotFound from "./routes/NotFound";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root></Root>,
+        errorElement: <NotFound></NotFound>,
         children: [
             {
                 path: "",
                 element: <Home/>,
             },
-            {
-                path: "users",
-                element: <Users/>,
-            }
+
         ]
     }
 ])
