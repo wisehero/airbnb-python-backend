7af32251ab186face171de766a3a7dd0eec03a54
wisehero10 <godjw96@gmail.com>
2022-11-26 22:45:31 +0900

    # 차크라 UI 테스트

--- airbnb-clone-frontend/src/App.tsx
+++ airbnb-clone-frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Text} from "@chakra-ui/react";
 
 
 function App() {
-    return <div></div>;
+    return <div>
+        <Text color={"red.600"} fontSize={"6xl"}>It works!!!</Text>
+    </div>;
 }
 
 export default App;
