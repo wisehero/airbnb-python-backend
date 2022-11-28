20a673b9f16616f9cec7dd2718eaeed53c1afd1c
wisehero10 <godjw96@gmail.com>
2022-11-28 20:09:43 +0900

    # 헤더 만들기

--- airbnb-clone-frontend/src/components/Root.tsx
+++ airbnb-clone-frontend/src/components/Root.tsx
@@ -1,10 +1,26 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import {Box, Button, HStack} from "@chakra-ui/react";
+import {FaAirbnb} from "react-icons/fa";
 
 export default function Root() {
     return (
-        <h1>
-            im root
+        <Box>
+            <HStack
+                justifyContent={"space-between"}
+                py={5}
+                px={10}
+                borderBottomWidth={1}>
+                <Box color="red.500">
+                    <Link to={"/"}>
+                        <FaAirbnb size={"48"}/>
+                    </Link>
+                </Box>
+                <HStack>
+                    <Button>Log in</Button>
+                    <Button colorScheme={"red"}>Sign Up</Button>
+                </HStack>
+            </HStack>
             <Outlet/>
-        </h1>
-    )
+        </Box>
+    );
 }
